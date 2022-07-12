4cc8a7ff55b21dbd8bd793db1639e2b404611c95
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-12 20:10:15 +0530

    Add files via upload

---
+++ doc7.py
@@ -0,0 +1,10 @@
+
+monument=input('enter any monument')
+if monument=='Red fort':
+	print('Delhi')
+elif monument=='Taj Mahal':
+	print('Agra')
+elif monument=='Jai Mahal':
+	print('Jaipur')
+else:
+	print('Nothing')
